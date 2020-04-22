3692dc70fa653ad69ecccc43b4b69e4133e31a60
Wijesinghe, Thanuja Chamika <thanujachamika.wijesinghe@pearson.com>
2020-04-22 14:25:14 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactVideoTrimmer from "react-video-trimmer";
+import "react-video-trimmer/dist/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <ReactVideoTrimmer timeLimit={600} showEncodeBtn />
+      </div>
   );
 }
+
+const Trimmer = () => {
+    return (
+        <div>
+            <ReactVideoTrimmer timeRange={20} />
+        </div>
+    );
+};
 
 export default App;
