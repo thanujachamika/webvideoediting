1cc7f48612be528726629802fd248f1b791721fa
Wijesinghe, Thanuja Chamika <thanujachamika.wijesinghe@pearson.com>
2020-04-27 18:31:49 +0530

    Webcam recording added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable */
+import React, { Component } from 'react';
+
 import './App.css';
+
 import ReactVideoTrimmer from "react-video-trimmer";
 import "react-video-trimmer/dist/style.css";
 
-function App() {
-  return (
-      <div>
-          <ReactVideoTrimmer timeLimit={600} showEncodeBtn />
-      </div>
-  );
-}
+import 'video.js/dist/video-js.css';
+import videojs from 'video.js';
 
-const Trimmer = () => {
-    return (
-        <div>
-            <ReactVideoTrimmer timeRange={20} />
-        </div>
-    );
-};
+import 'webrtc-adapter';
+import RecordRTC from 'recordrtc';
+
+/*
+// Required imports when recording audio-only using the videojs-wavesurfer plugin
+import WaveSurfer from 'wavesurfer.js';
+import MicrophonePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.microphone.js';
+WaveSurfer.microphone = MicrophonePlugin;
+
+// Register videojs-wavesurfer plugin
+import 'videojs-wavesurfer/dist/css/videojs.wavesurfer.css';
+import Wavesurfer from 'videojs-wavesurfer/dist/videojs.wavesurfer.js';
+*/
+
+// register videojs-record plugin with this import
+import 'videojs-record/dist/css/videojs.record.css';
+import Record from 'videojs-record/dist/videojs.record.js';
+
+// Optional imports for videojs-record plugins
+/*
+// webm-wasm plugin (npm install webm-wasm @mattiasbuelens/web-streams-polyfill)
+// Make sure to copy webm-worker.js and webm-wasm.wasm from
+// node_modules/webm-wasm/dist/ to the project's public directory
+import '@mattiasbuelens/web-streams-polyfill/dist/polyfill.min.js';
+import 'videojs-record/dist/plugins/videojs.record.webm-wasm.js';
+
+// ts-ebml plugin (npm install ts-ebml)
+import 'videojs-record/dist/plugins/videojs.record.ts-ebml.js';
+*/
+
+class App extends Component {
+    componentDidMount() {
+        // instantiate Video.js
+        this.player = videojs(this.videoNode, this.props, () => {
+            // print version information at startup
+            var version_info = 'Using video.js ' + videojs.VERSION +
+                ' with videojs-record ' + videojs.getPluginVersion('record') +
+                ' and recordrtc ' + RecordRTC.version;
+            videojs.log(version_info);
+        });
+
+        // device is ready
+        this.player.on('deviceReady', () => {
+            console.log('device is ready!');
+        });
+
+        // user clicked the record button and started recording
+        this.player.on('startRecord', () => {
+            console.log('started recording!');
+        });
+
+        // user completed recording and stream is available
+        this.player.on('finishRecord', () => {
+            // recordedData is a blob object containing the recorded data that
+            // can be downloaded by the user, stored on server etc.
+            console.log('finished recording: ', this.player.recordedData);
+        });
+
+        // error handling
+        this.player.on('error', (element, error) => {
+            console.warn(error);
+        });
+
+        this.player.on('deviceError', () => {
+            console.error('device error:', this.player.deviceErrorCode);
+        });
+    }
+
+    // destroy player on unmount
+    componentWillUnmount() {
+        if (this.player) {
+            this.player.dispose();
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h2>Record video : </h2>
+                <div data-vjs-player>
+                    <video id="myVideo" ref={node => this.videoNode = node} className="video-js vjs-default-skin"
+                           playsinline></video>
+                </div>
+
+                <div>
+                    <h2>Trim video : </h2>
+                    <ReactVideoTrimmer
+                        timeLimit={600}
+                        showEncodeBtn
+                        loadingFFMPEGText="Loading required libs..."
+                        timeRange={5}
+                        maxSize={100000240}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {RdxVideo, Overlay, Controls} from 'react-html5-video-editor';
+
+const videoJsOptions = {
+    controls: true,
+    width: 320,
+    height: 240,
+    fluid: false,
+    plugins: {
+        record: {
+            audio: true,
+            video: true,
+            maxLength: 3600,
+            debug: true,
+        }
+    }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App { ...videoJsOptions }/>
   </React.StrictMode>,
   document.getElementById('root')
 );
